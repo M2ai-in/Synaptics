7457db04956c60dfdc0383f45efb9835c50406c8
Ram Kadiyala <mimmu1024@gmail.com>
2025-05-07 00:41:48 -0400

    Update planner_tool.py

--- agentpro/tools/planner_tool.py
+++ agentpro/tools/planner_tool.py
@@ -1,6 +1,5 @@
 from agentpro.tools.base import Tool
 from pydantic import PrivateAttr
-
 class PlannerTool(Tool):
     name: str = "planner_tool"
     description: str = (
@@ -8,22 +7,17 @@ class PlannerTool(Tool):
         "and plans learning paths."
     )
     arg: str = "Instruction like 'plan deep learning', 'recommend videos on transformers', or 'summarize a topic'."
-
     _sub_agent = PrivateAttr()
-
     def __init__(self, sub_agent):
         super().__init__()
         self._sub_agent = sub_agent
-
     def run(self, prompt: str) -> str:
         print(f"🛠️ Calling Planner Tool with topic: {prompt}")
         intent, _, topic = prompt.partition(" ")
         intent = intent.strip().lower()
         topic = topic.strip()
-
         if not topic:
             return "Please provide a topic along with the instruction."
-
         if intent in ["plan", "learning", "learn"]:
             print("Planning learning for topic:", topic)
             return self.plan_learning(topic)
@@ -38,23 +32,18 @@ class PlannerTool(Tool):
                 "I didn't understand that. Try: 'plan deep learning', "
                 "'recommend videos on transformers', or 'summarize quantum mechanics'."
             )
-
     def plan_learning(self, topic: str) -> str:
         prompt = f"Break down the topic '{topic}' into a step-by-step learning plan with subtopics."
         result= self._sub_agent(prompt)
         print("The result is :", result)
         return result
-
     def recommend_videos(self, topic: str) -> str:
         prompt = f"Find top YouTube videos to learn about {topic} effectively."
-        # return self._sub_agent(prompt)
         result= self._sub_agent(prompt)
         print("The result is :", result)
         return result
-
     def summarize_and_note(self, topic: str) -> str:
         prompt = f"Search and summarize key points from online resources and make concise notes on {topic}."
-        # return self._sub_agent(prompt)
         result= self._sub_agent(prompt)
         print("The result is :", result)
         return result
