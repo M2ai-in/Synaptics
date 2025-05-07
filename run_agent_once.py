add1c8cc80b7f176e54f767960393f6ce894ea2b
Ram Kadiyala <mimmu1024@gmail.com>
2025-05-07 00:36:12 -0400

    Update run_agent_once.py

--- run_agent_once.py
+++ run_agent_once.py
@@ -3,13 +3,9 @@ from agentpro.tools import (AresInternetTool, CodeEngine, YouTubeSearchTool,Slid
 import os
 import dotenv
 import numpy as np
-
-
 def run_agent_once(prompt: str, temperature: float = 0.4, max_tokens: int = 4000):
     dotenv.load_dotenv()
-
     use_openrouter = os.getenv("OPENROUTER_API_KEY") is not None
-
     if use_openrouter:
         print('Using OpenRouter API')
         client_details = {
@@ -25,7 +21,6 @@ def run_agent_once(prompt: str, temperature: float = 0.4, max_tokens: int = 4000
         if not os.environ.get("OPENAI_API_KEY"):
             print("Error: OPENAI_API_KEY environment variable is not set.")
             return
-
         client_details = {
             "api_key": os.getenv("OPENAI_API_KEY"),
             "api_base": "https://api.openai.com/v1/",
@@ -35,19 +30,13 @@ def run_agent_once(prompt: str, temperature: float = 0.4, max_tokens: int = 4000
         CODE_MODEL="gpt-4o-mini"
         YT_MODEL="gpt-4o-mini"
         SLIDE_MODEL="gpt-4o-mini"
-
     code_tool = CodeEngine(client_details=client_details, model_name=CODE_MODEL)
     youtube_tool = YouTubeSearchTool(client_details=client_details, model_name=YT_MODEL)
     slide_tool = SlideGenerationTool(client_details=client_details, model_name=SLIDE_MODEL)
-
-    common_tools = [
-        code_tool, youtube_tool, slide_tool
-    ]
+    common_tools = [code_tool, youtube_tool, slide_tool]
     tools = common_tools.copy()
     if os.environ.get("TRAVERSAAL_ARES_API_KEY"):
         ares_tool = AresInternetTool(client_details=client_details)
         tools.append(ares_tool)
-
     agent = AgentPro(tools=tools, client_details=client_details if use_openrouter else None, temperature=temperature, max_tokens=max_tokens)
-    
     return agent(prompt)
