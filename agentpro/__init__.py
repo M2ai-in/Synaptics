456f053d9ae0d20e41f8decf07bc23b9e2d13cce
Ram Kadiyala <mimmu1024@gmail.com>
2025-05-13 12:15:37 -0400

    Update __init__.py

--- agentpro/__init__.py
+++ agentpro/__init__.py
@@ -1,8 +1,4 @@
 from .agent import AgentPro
 from typing import Any
 from agentpro.tools import AresInternetTool, CodeEngine, YouTubeSearchTool, SlideGenerationTool, DataScienceTool # add more tools when available
-# # ares_tool = AresInternetTool()
-# code_tool = CodeEngine()
-# youtube_tool = YouTubeSearchTool()
-# slide_tool = SlideGenerationTool()
-__all__ = ['AgentPro', 'ares_tool', 'code_tool', 'youtube_tool', 'slide_tool','data_tool'] # add more tools when available
+__all__ = ['AgentPro', 'ares_tool', 'code_tool', 'youtube_tool', 'slide_tool', 'data_tool'] # add more tools when available
